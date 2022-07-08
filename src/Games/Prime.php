206e81b8cacf94502906e244975d07fe4407a910
Stanislav Shein <firmvw@gmail.com>
2022-07-08 21:57:28 +0300

    Update game files

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,8 +8,8 @@ use const Brain\Engine\NUMBER_OF_ROUNDS;
 
 function isPrime(int $number): bool
 {
-    if ($number === 1 || $number === 2) {
-        return true;
+    if ($number === 1) {
+        return false;
     }
     for ($check = 2; $check <= sqrt($number); $check++) {
         if ($number % $check === 0) {
@@ -24,7 +24,7 @@ function play(): void
     $task = 'Answer "yes" if the number is prime. Otherwise answer "no".';
     $gameData = [];
     for ($i = 0; $i < NUMBER_OF_ROUNDS; $i++) {
-        $number = rand(1, 5);
+        $number = rand(1, 200);
         if (isPrime($number)) {
             $rightAnswer = 'yes';
         } else {
